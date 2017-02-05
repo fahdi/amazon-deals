c999ce50d2f196847b596148caa375c5e734c028
Fahad Murtaza <fahdim@gmail.com>
2017-02-05 23:24:11 +0500

    Adding configuration management via convict

---
+++ config/index.js
@@ -0,0 +1,44 @@
+var convict = require('convict');
+
+// Define a schema
+var config = convict({
+  env: {
+    doc: "The applicaton environment.",
+    format: ["production", "development", "test"],
+    default: "development",
+    env: "NODE_ENV"
+  },
+  ip: {
+    doc: "The IP address to bind.",
+    format: "ipaddress",
+    default: "127.0.0.1",
+    env: "IP_ADDRESS",
+  },
+  port: {
+    doc: "The port to bind.",
+    format: "port",
+    default: 3000,
+    env: "PORT"
+  },
+  database: {
+    host: {
+      doc: "Database host name/IP",
+      format: '*',
+      default: 'localhost:27017'
+    },
+    name: {
+      doc: "Database name",
+      format: String,
+      default: 'amazondeals'
+    }
+  }
+});
+
+// Load environment dependent configuration
+var env = config.get('env');
+config.loadFile('./config/' + env + '.json');
+
+// Perform validation
+config.validate({allowed: 'strict'});
+
+module.exports = config;
