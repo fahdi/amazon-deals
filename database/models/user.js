2aa84e7965a8609cb4bdcfae43ffacdfe2aab322
Fahad Murtaza <fahdim@gmail.com>
2017-02-08 00:36:28 +0500

    Deleting unnecessary stuff

--- database/models/user.js
+++ database/models/user.js
@@ -17,16 +17,6 @@ var userSchema = new Schema({
   updated_at: Date
 });
 
-// custom method to add string to end of name
-// you can create more important methods like name validations or formatting
-// you can also do queries and find similar users 
-userSchema.methods.dudify = function() {
-  // add some stuff to the users name
-  this.name = this.name + '-dude'; 
-
-  return this.name;
-};
-
 // on every save, add the date
 userSchema.pre('save', function(next) {
   // get the current date
